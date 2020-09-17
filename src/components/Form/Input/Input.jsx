1341e79d8dd4c0970e62fb092f3483ebd2edcee0
lemonreds <lemonreds@163.com>
2020-09-17 17:21:09 +0800

    fix focusing

--- src/components/Form/Input/Input.jsx
+++ src/components/Form/Input/Input.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import useValueChange from 'hooks/useValueChange';
 import styles from './Input.less';
 
 function Input(props) {
+  const ref = useRef();
   const [value, setValue] = useValueChange(props);
   const { placeholder, style } = props;
 
   const onChange = e => {
     setValue(e.target.value);
   };
+  const toFocus = e => {
+    ref.current.focus();
+    e.stopPropagation();
+  };
   return (
     <div className={styles.container} style={style}>
       <input
+        ref={ref}
         type="input"
         className={styles.input}
         value={value || ''}
@@ -19,7 +25,9 @@ function Input(props) {
         placeholder="placeholder"
       />
       <div className={styles.line} />
-      <div className={styles.label}>{placeholder}</div>
+      <div className={styles.label} onClick={toFocus}>
+        {placeholder}
+      </div>
     </div>
   );
 }
